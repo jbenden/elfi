df3ec41467c6db16de05c410b3d7e6081ae2ac94
Jarno Lintusaari <jarno.lintusaari@aalto.fi>
2016-10-27 11:59:49 +0300

    intro_to_abc notebook

--- elfi/examples/ma2.py
+++ elfi/examples/ma2.py
@@ -12,12 +12,28 @@ def MA2(n, N, t1, t2, prng=None, latents=None):
 
 
 def autocov(lag, y):
-    y = (y - np.mean(y, axis=1, keepdims=True)) / np.var(y, axis=1, keepdims=True)
-    tau = np.sum(y[:,lag:] * y[:,:-lag], axis=1, keepdims=True)
+    # weak stationary version
+    y = (y - np.mean(y, axis=1, keepdims=True))
+    tau = np.mean(y[:,lag:] * y[:,:-lag], axis=1, keepdims=True) / np.var(y, axis=1, keepdims=True)
     return tau
+
+    # no stationary assumptions
+    #X_t = y[:,:-lag]
+    #X_s = y[:,lag:]
+    #mu_t = np.mean(X_t, axis=1, keepdims=True)
+    #mu_s = np.mean(X_s, axis=1, keepdims=True)
+    #C = np.mean(X_t * X_s, axis=1, keepdims=True) - mu_t*mu_s
+    # Autocorrelation
+    #tau = C / np.var(y, axis=1, keepdims=True)
+    #return tau
+
+    # Original
+    #y = (y - np.mean(y, axis=1, keepdims=True)) / np.var(y, axis=1, keepdims=True)
+    #tau = np.sum(y[:,lag:] * y[:,:-lag], axis=1, keepdims=True)
+    #return tau
 
 
 def distance(x, y):
-    d = np.linalg.norm( np.array(x) - np.array(y), ord=2, axis=0)
+    d = np.linalg.norm( np.array(x) - np.array(y), ord=1, axis=0)
     return d
 
